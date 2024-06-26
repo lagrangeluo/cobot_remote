cf0a4f8029ef3a8d725a6c0180e576b8e87ed295
lagrangeluo <654186749@qq.com>
2024-06-26 20:29:00 +0800

    complete track

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cobot Remote for AgileX Robotics
--- vive_ros/src/survive_ros.cpp
+++ vive_ros/src/survive_ros.cpp
@@ -6,6 +6,7 @@ void survive_ros_node::init()
     
     ros::NodeHandle nh("~");
 
+    nh.getParam("world_name",world_name);
     nh.getParam("tracker_left",tracker_left);
     nh.getParam("tracker_right", tracker_right);
     nh.getParam("teleop_base", teleop_base);
@@ -20,7 +21,8 @@ void survive_ros_node::init()
     // init tracker base transform
     tracker_static={};
     tracker_static.header.stamp = ros::Time::now();
-    tracker_static.child_frame_id = world_name;
+    tracker_static.header.frame_id = world_name;
+    tracker_static.child_frame_id = teleop_base;
     tracker_static.transform.rotation.w = 1;
 
 }
@@ -38,15 +40,25 @@ void survive_ros_node::joy_topic_callback(const sensor_msgs::Joy::ConstPtr msg)
             try{
                 listener.lookupTransform(world_name,tracker_left,ros::Time(0),trans);
                 tf::Quaternion q = trans.getRotation();
+
+                // get r p y and only use y angle
+                double roll, pitch,yaw;
+                tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+                roll = 0;
+                pitch = 0;
+
+                tf::Quaternion new_q;
+                new_q.setRPY(roll, pitch, yaw);
+                ROS_INFO("Tracker Base RPY: roll=%f, pitch=%f, yaw=%f", roll, pitch, yaw);
                 
                 tracker_static.header.stamp = ros::Time::now();
                 tracker_static.transform.translation.x = trans.getOrigin().x();
                 tracker_static.transform.translation.y = trans.getOrigin().y();
                 tracker_static.transform.translation.z = trans.getOrigin().z();
-                tracker_static.transform.rotation.x = q.x();
-                tracker_static.transform.rotation.y = q.y();
-                tracker_static.transform.rotation.z = q.z();
-                tracker_static.transform.rotation.w = q.w();
+                tracker_static.transform.rotation.x = new_q.x();
+                tracker_static.transform.rotation.y = new_q.y();
+                tracker_static.transform.rotation.z = new_q.z();
+                tracker_static.transform.rotation.w = new_q.w();
             }
             catch(tf::TransformException &ex){
                 ROS_ERROR("%s", ex.what());
@@ -62,10 +74,22 @@ void survive_ros_node::check_start_teleop()
 {
     if(start_teleop == true)
     {
+        // pub tracker static tf
+        tracker_static.header.stamp = ros::Time::now();
+        broadcaster.sendTransform(tracker_static);
         
         tf::StampedTransform trans;
         try{
             listener.lookupTransform(teleop_base,tracker_left,ros::Time(0),trans);
+            // ROS_INFO("Translation: x=%f, y=%f, z=%f", trans.getOrigin().x(), 
+            //                 trans.getOrigin().y(), trans.getOrigin().z());
+                        
+            // get r p y and only use y angle
+            double roll, pitch,yaw;
+            tf::Quaternion q = trans.getRotation();
+            tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+            // ROS_INFO("RPY: roll=%f, pitch=%f, yaw=%f",roll,pitch,yaw);
+
         }
         catch(tf::TransformException &ex){
             ROS_ERROR("%s", ex.what());
